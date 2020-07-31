fe246416ea5f47196e67a1290099346025cdcd94
Pablo Mendez <49279685+Elsacacorchos@users.noreply.github.com>
2020-07-30 21:50:20 -0300

    Add files via upload
    Capa de Acceso a Datos

---
+++ DatosDiarioADiario/ConnectionToSql.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data.SqlClient
+
+Public MustInherit Class ConnectionToSql
+    Public connectionString As String
+    Protected Sub New()
+        connectionString = "Data Source=DESKTOP-GL2VC9T\SQLSERVER;Initial Catalog=DDBD;Persist Security Info=True;User ID=sa;Password=54325432a"
+
+    End Sub
+    Protected Function GetConnection() As SqlConnection
+        Return New SqlConnection(connectionString)
+    End Function
+End Class
+
